ec05a5c1c6d401f78aa8703230eb29517df605fd
NeilMathew33 <mathew.neil33@gmail.com>
2026-05-03 19:46:57 -0700

    added input

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,37 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>
 
-<div className="container">
-    <div className="row justify-content-center vh-100 align-items-center">
-        <div className="col col-12 col-sm-8 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-            
-<div class="card shadow">
-  <div class="card-body">
-    <h4 class="text-center">User Login</h4>
-    <div className="mb-3">
-        <label htmlFor="" className="form-label">Username</label>
-        <input type="text" className="form-control" />
-    </div>
-    <div className="mb-3">
-        <label htmlFor="" className="form-label">Password</label>
-        <input type="text" className="form-control" />
-    </div>
-    <div className="mb-3">
-        <button className="btn btn-success">Login</button>
-    </div>
-    <a href="/SignUp">New Users Click Here</a>
-  </div>
-</div>
+    const [input, setInput]=useState(
+        {
+            username:"",
+            password:"",
+        }
+    )
+
+const handleInput = (event) => {
+setInput(
+    { ...input, [event.target.name]:event.target.value}
+)
+}
+
+const readValues= () => {
+    console.log(input)
+}
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row justify-content-center vh-100 align-items-center">
+                    <div className="col col-12 col-sm-8 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="text-center">User Login</h4>
+                                <div className="mb-3">
+                                    <label htmlFor="" className="form-label">Username</label>
+                                    <input type="text" className="form-control" name="username" value={input.username} onChange={handleInput} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="" className="form-label">Password</label>
+                                    <input type="password" name="password" value={input.password} onChange={handleInput} className="form-control" />
+                                </div>
+                                <div className="mb-3">
+                                    <button onClick={readValues} className="btn btn-success">Login</button>
+                                </div>
+                                <div className="mb-3">
+                                    <a href="/SignUp">New Users Click Here</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
 
         </div>
-    </div>
-</div>
-
-    </div>
-  )
+    )
 }
 
 export default Login
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,60 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+
+const [input, setInput] = useState(
+    {
+      firstname: "",
+      middlename: "",
+      lastname: "",
+      username: "",
+      phonenumber: "",
+      departmentorgroup: "",
+      emailid: "",
+      profilepicture: "",
+      password: "",
+      confirmpassword: "",
+    }
+  )
+  
+  const handleInput = (event) => {
+    setInput(
+      {... input,[event.target.name]:event.target.value}
+    )
+  }
+
+  const readValues = () => {
+    console.log(input)
+  }
+
   return (
     <div>
 
 <div className="container">
   <div className="row justify-content-center vh-100 align-items-center">
     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xxl-12">
-  <div class="card shadow">
+      <div class="card shadow">
+ 
   <div class="card-body ">
+    
 <div className="row g-3">
 <h4 className='text-center'>User Sign Up</h4>
 
 
         <div className="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
           <label htmlFor="" className="form-label">First Name</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="firstname" value={input.firstname} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
           <label htmlFor="" className="form-label">Middle Name</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="middlename" value={input.middlename} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
           <label htmlFor="" className="form-label">Last Name</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="lastname" value={input.lastname} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
           <label htmlFor="" className="form-label">Username</label>
-         <input type="text" className="form-control" />
+         <input type="text" className="form-control" name="username" value={input.username} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Phone Number</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="phonenumber" value={input.phonenumber} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Department or Group</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="departmentorgroup" value={input.departmentorgroup} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Email ID</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="emailid" value={input.emailid} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Profile Picture</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="profilepicture" value={input.profilepicture} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Password</label>
-          <input type="password" name="" id="" className="form-control" />
+          <input type="password" name="password" id="" className="form-control" value={input.password} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Confirm Password</label>
-          <input type="password" name="" id="" className="form-control" />
+          <input type="password" name="confirmpassword" id="" className="form-control" value={input.confirmpassword} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-          <button className="btn btn-success">Sign Up</button>
+          <button onClick={readValues} className="btn btn-success">Sign Up</button>
         </div>
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
           <a href="/"> I already have an account</a>
